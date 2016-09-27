fd997a526ea3c7cf1ee5d9aaa515d34da3bbcf12
tt <69443245@qq.com>
2016-09-28 00:59:02 +0800

    Merge branch 'master' of https://github.com/luopotaotao/staticLoad
    # Conflicts:
    #	test.iml

--- src/main/java/tt/controller/RoleController.java
+++ src/main/java/tt/controller/RoleController.java
@@ -4,6 +4,7 @@ package tt.controller;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -26,13 +27,12 @@ import tt.util.annotation.ValidatorAnnotationBean;
 
 /**
  * 角色控制器
- * 
+ *
  * @author
  */
 @Controller
 @RequestMapping("/roleController")
-public class RoleController extends BaseController
-{
+public class RoleController extends BaseController {
     private static final Logger logger = Logger.getLogger(RoleController.class);
 
     @Autowired
@@ -40,57 +40,56 @@ public class RoleController extends BaseController
 
     /**
      * 跳转到角色管理页面
-     * 
+     *
      * @return
      */
     @RequestMapping("/manager")
-    public String manager()
-    {
-        this.getSessionInfo().getResourceList().add("/roleController/treeGrid.action");
-        this.getSessionInfo().getResourceMap().put("/roleController/treeGrid.action", "角色表格");
+    public String manager() {
+        String url = "/roleController/treeGrid.action";
+        Pattern regex = Pattern.compile(url);
+        this.getSessionInfo().getResourceList().add(regex);
+        this.getSessionInfo().getResourceMap().put(url, "角色表格");
         return "/admin/role/role";
     }
 
     /**
      * 跳转到角色添加页面
-     * 
+     *
      * @return
      */
     @RequestMapping("/addPage")
-    public String addPage(HttpServletRequest request)
-    {
+    public String addPage(HttpServletRequest request) {
         Role r = new Role();
         r.setId(UUID.randomUUID().toString());
-        this.getSessionInfo().getResourceList().add("/roleController/add.action");
-        this.getSessionInfo().getResourceMap().put("/roleController/add.action", "角色添加功能");
+        String url = "/roleController/add.action";
+        Pattern regex = Pattern.compile(url);
+
+        this.getSessionInfo().getResourceList().add(regex);
+        this.getSessionInfo().getResourceMap().put(url, "角色添加功能");
         request.setAttribute("role", r);
         return "/admin/role/roleAdd";
     }
 
     /**
      * 添加角色
-     * 
+     *
      * @return
      */
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     @ResponseBody
     public Json add(Role role, HttpSession session)
-        throws Exception
-    {
-        SessionInfo sessionInfo = (SessionInfo)session.getAttribute(
-            ConfigUtil.getSessionInfoName());
+            throws Exception {
+        SessionInfo sessionInfo = (SessionInfo) session.getAttribute(
+                ConfigUtil.getSessionInfoName());
         Json j = new Json();
         List<String> validateList = new ArrayList<String>();
         validateList.add("id");
         validateList.add("name");
         validateList.add("remark");
-        if (ValidatorAnnotationBean.validatorBeanParams(role, validateList))
-        {
+        if (ValidatorAnnotationBean.validatorBeanParams(role, validateList)) {
             j = roleService.add(role, sessionInfo);
             logger.info("添加角色" + role.getName() + "成功！");
-        }
-        else
-        {
+        } else {
             j.setSuccess(false);
             j.setMsg("添加失败(属性校验失败)！");
             logger.info("添加角色失败：(属性校验失败)！");
@@ -100,23 +99,22 @@ public class RoleController extends BaseController
 
     /**
      * 跳转到角色修改页面
-     * 
+     *
      * @return
      */
     @RequestMapping(value = "/editPage", method = RequestMethod.POST)
     public String editPage(HttpServletRequest request, String id)
-        throws Exception
-    {
+            throws Exception {
         Role r = roleService.get(id);
-        if (r == null)
-        {
+        if (r == null) {
             logger.info("该角色信息不存在，请刷新页面！");
             return "/error/noInfo";
-        }
-        else
-        {
-            this.getSessionInfo().getResourceList().add("/roleController/edit.action");
-            this.getSessionInfo().getResourceMap().put("/roleController/edit.action", "角色编辑功能");
+        } else {
+            String url = "/roleController/edit.action";
+            Pattern regex = Pattern.compile(url);
+
+            this.getSessionInfo().getResourceList().add(regex);
+            this.getSessionInfo().getResourceMap().put(url, "角色编辑功能");
             request.setAttribute("role", r);
             return "/admin/role/roleEdit";
         }
@@ -125,35 +123,28 @@ public class RoleController extends BaseController
 
     /**
      * 修改角色
-     * 
+     *
      * @param role
      * @return
      */
     @RequestMapping(value = "/edit", method = RequestMethod.POST)
     @ResponseBody
     public Json edit(Role role)
-        throws Exception
-    {
+            throws Exception {
         Json j = new Json();
         List<String> validateList = new ArrayList<String>();
         validateList.add("name");
         validateList.add("remark");
-        if (ValidatorAnnotationBean.validatorBeanParams(role, validateList))
-        {
+        if (ValidatorAnnotationBean.validatorBeanParams(role, validateList)) {
             Role r = roleService.get(role.getId());
-            if (r == null)
-            {
+            if (r == null) {
                 j.setSuccess(false);
                 j.setMsg("修改失败，该角色信息不存在，请刷新页面！");
                 logger.info("该角色信息【" + role.getName() + "】不存在，请刷新页面！");
-            }
-            else
-            {
+            } else {
                 j = roleService.edit(role);
             }
-        }
-        else
-        {
+        } else {
             j.setSuccess(false);
             j.setMsg("修改失败(属性校验失败)！");
             logger.error("修改角色:" + role.getName() + "【" + role.getId() + "】失败(属性校验失败)！");
@@ -164,71 +155,66 @@ public class RoleController extends BaseController
 
     /**
      * 获得角色列表
-     * 
+     *
      * @return
      */
     @RequestMapping(value = "/treeGrid", method = RequestMethod.POST)
     @ResponseBody
     public List<Role> treeGrid(HttpSession session)
-        throws Exception
-    {
-        SessionInfo sessionInfo = (SessionInfo)session.getAttribute(
-            ConfigUtil.getSessionInfoName());
+            throws Exception {
+        SessionInfo sessionInfo = (SessionInfo) session.getAttribute(
+                ConfigUtil.getSessionInfoName());
         return roleService.treeGrid(sessionInfo);
     }
 
     /**
      * 角色树(只能看到自己拥有的角色)
-     * 
+     *
      * @return
      */
     @RequestMapping(value = "/tree", method = RequestMethod.POST)
     @ResponseBody
     public List<Tree> tree(HttpSession session)
-        throws Exception
-    {
-        SessionInfo sessionInfo = (SessionInfo)session.getAttribute(
-            ConfigUtil.getSessionInfoName());
+            throws Exception {
+        SessionInfo sessionInfo = (SessionInfo) session.getAttribute(
+                ConfigUtil.getSessionInfoName());
         return roleService.tree(sessionInfo);
     }
 
     /**
      * 角色树
-     * 
+     *
      * @return
      */
     @RequestMapping("/allTree")
     @ResponseBody
     public List<Tree> allTree()
-        throws Exception
-    {
+            throws Exception {
         return roleService.allTree();
     }
 
     /**
      * 授权角色树(不包含超级管理员角色)
-     * 
+     *
      * @return
      */
     @RequestMapping("/grantRoleTree")
     @ResponseBody
     public List<Tree> grantTree()
-        throws Exception
-    {
+            throws Exception {
         return roleService.grantRoleTree();
     }
 
     /**
      * 删除角色
-     * 
+     *
      * @param id
      * @return
      */
     @RequestMapping(value = "/delete", method = RequestMethod.POST)
     @ResponseBody
     public Json delete(String id)
-        throws Exception
-    {
+            throws Exception {
         Json j = new Json();
         roleService.delete(id);
         j.setMsg("删除成功！");
@@ -238,27 +224,26 @@ public class RoleController extends BaseController
 
     /**
      * 跳转到角色授权页面
-     * 
+     *
      * @return
      */
     @RequestMapping(value = "/grantPage", method = RequestMethod.POST)
     public String grantPage(HttpServletRequest request, String id)
-        throws Exception
-    {
+            throws Exception {
         Role r = roleService.get(id);
-        if (r == null)
-        {
+        if (r == null) {
             logger.error("该角色信息不存在，请刷新页面！");
             return "/error/noInfo";
-        }
-        else
-        {
-            this.getSessionInfo().getResourceList().add("/resourceController/allTree.action");
-            this.getSessionInfo().getResourceMap().put("/resourceController/allTree.action",
-                "角色授权-资源下拉列表");
-            this.getSessionInfo().getResourceList().add("/roleController/grant.action");
-            this.getSessionInfo().getResourceMap().put("/roleController/grant.action",
-                "角色授权功能");
+        } else {
+            String allTreeUrl = "/resourceController/allTree.action";
+            String grantUrl = "grant";
+            Pattern allTreeRegex = Pattern.compile(allTreeUrl);
+            Pattern grantRegex = Pattern.compile(grantUrl);
+
+            this.getSessionInfo().getResourceList().add(allTreeRegex);
+            this.getSessionInfo().getResourceMap().put(allTreeUrl, "角色授权-资源下拉列表");
+            this.getSessionInfo().getResourceList().add(grantRegex);
+            this.getSessionInfo().getResourceMap().put(grantUrl, "角色授权功能");
             request.setAttribute("role", r);
             return "/admin/role/roleGrant";
         }
@@ -266,25 +251,21 @@ public class RoleController extends BaseController
 
     /**
      * 授权
-     * 
+     *
      * @param role
      * @return
      */
     @RequestMapping(value = "/grant", method = RequestMethod.POST)
     @ResponseBody
     public Json grant(Role role)
-        throws Exception
-    {
+            throws Exception {
         Json j = new Json();
         Role r = roleService.get(role.getId());
-        if (r == null)
-        {
+        if (r == null) {
             j.setMsg("授权失败，该角色信息不存在，请刷新页面！！");
             j.setSuccess(false);
             logger.error("该角色信息不存在，请刷新页面！");
-        }
-        else
-        {
+        } else {
             roleService.grant(role);
             j.setMsg("授权成功！");
             j.setSuccess(true);
